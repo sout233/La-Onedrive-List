89f07c591a8679d443a47262bde79a2c090adaf3
sout_Nantang <55623894+sout233@users.noreply.github.com>
2023-10-01 13:24:35 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,5 @@ module.exports = {
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
   trailingSlash: true
+  target: 'experimental-serverless-trace'
 }
